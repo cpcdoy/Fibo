37abc5d4b13ccb55192e9a05de54d1a922feab99
cpcdoy <cpcdoy@gmail.com>
2019-05-21 23:24:24 +0200

    Add: header for the new fibo computation with no loops

--- src/fibo/fibo.hh
+++ src/fibo/fibo.hh
@@ -5,8 +5,9 @@ class fibo
 	public:
 		fibo() { }
 
-		long long int get_even_sum(int n);
+		long long int get_even_sum(int n, bool no_loops = true);
 
 	private:
 		long long int even_sum(int n);
+		long long int even_sum_no_loop(int n);
 };
